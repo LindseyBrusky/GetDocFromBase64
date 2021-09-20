6f2fca22fdae52bf1635220dc93449e2f4ccad2f
Lindsey Brusky <bruskylindsey@co.kane.il.us>
2021-09-20 10:38:04 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace GetDocFromBase64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string base64BinaryStr = args[1];
+            byte[] bytes = Convert.FromBase64String(base64BinaryStr);
+            System.IO.FileStream stream = new FileStream(args[0], FileMode.Create);
+            System.IO.BinaryWriter writer = new BinaryWriter(stream);
+            writer.Write(bytes, 0, bytes.Length);
+            writer.Close();
+            Console.WriteLine("Document Created at " + args[0]);
+        }
+    }
+}
